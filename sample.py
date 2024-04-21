29d878e5bc0762a98ad19daa0e4b0cb4cd7f8e5a
Shubhashish Biswas <shubhashish.g@gmail.com>
2024-04-21 22:09:42 +0530

    Update sample.py

--- sample.py
+++ sample.py
@@ -11,18 +11,11 @@ def largest(n1, n2, n3):
     else:
         return "Error" 
 
-# Title
-st.title("Hello GeeksForGeeks !!!")
-
  
-# save the input text in the variable 'name'
-# first argument shows the title of the text input box
-# second argument displays a default text inside the text input area
 n1 = st.text_input("Enter First Number")
 n2 = st.text_input("Enter Second Number")
 n3 = st.text_input("Enter Third Number")
     
-# .title() is used to get the input text string
 if(st.button('Submit')):    
     result = largest(n1, n2, n3)
     st.success(result)
